52d14176643b82551c686277c4512e838692cd34
Amina Makhusseyeva <amina.makhusseyeva@gmail.com>
2021-12-15 15:31:57 +0400

    Regular expression to match strings containing both 'Odoo' and '#rules' completed

--- Regex/regex.rb
+++ Regex/regex.rb
@@ -1,10 +1,10 @@
-# def match?(words)
-#   if words =~ /^(?=.*Odoo)(?=.*#rules).*$/
-#     return true
-#   else
-#     return false
-#   end
-# end
+def match?(words)
+  if words =~ /^(?=.*Odoo)(?=.*#rules).*$/
+    return true
+  else
+    return false
+  end
+end
 
 # p match?("Odoo#rules") # true
 # p match?("#rules Odoo") # true
